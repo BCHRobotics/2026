6721da6873a89f572ff30a82fa487f059b7bdbe5
Tashvi Adhikari <tashvi.adhikari@gmail.com>
2026-01-24 14:12:14 -0500

    .

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -168,7 +168,7 @@ public final class Constants {
       // Camera 0 - Front
       new Transform3d(
         new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(34)),
-        new Rotation3d(0, 0, 180)
+        new Rotation3d(0, 0, 0)
       ),
       // Camera 1 - Back
       new Transform3d(
